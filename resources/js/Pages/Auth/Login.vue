73087db5cc0fc77805aba8443c2ccf0d1e9a0689
Lenfumé Julien <julabina@hotmail.fr>
2023-10-19 12:48:46 +0200

    login and register page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,94 +1,99 @@
-<script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
-
-defineProps({
-    canResetPassword: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
-});
-
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
-});
-
-const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
-</script>
-
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+    <Head title="Connexion" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <AuthenticatedLayout>
+        <template #header>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+        </template>
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+        <section class="w-full flex justify-center min-h-screen">
+            
+            <form @submit.prevent="submit" class="w-72">
+                <h1 class="font-bold text-2xl text-center mt-14 mb-16">Se connecter</h1>
+                <div>
+                    <InputLabel class="font-semibold" for="email" value="Email" />
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+                    <input
+                        id="email"
+                        type="email"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    />
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
+                <div class="mt-4">
+                    <InputLabel class="font-semibold" for="password" value="Mot de passe" />
 
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+                    <input
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                    />
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
+                <div class="block mt-4">
+                    <label class="flex items-center">
+                        <Checkbox name="remember" v-model:checked="form.remember" />
+                        <span class="ml-2 text-sm text-gray-600">Se souvenir de moi</span>
+                    </label>
+                </div>
 
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+                <button class="w-full btn-primary mt-6 text-xl h-10" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Se connecter
+                </button>
+
+                <div class="flex flex-col items-end justify-end mt-4">
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-2"
+                    >
+                        Mot de passe oublié ?
+                    </Link>
+                    <Link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Pas encore de compte ?
+                    </Link>
+                </div>
+            </form>
+        </section>
+    </AuthenticatedLayout>
 </template>
+
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head, Link, useForm } from '@inertiajs/vue3';
+    import Checkbox from '@/Components/Checkbox.vue';
+    import InputError from '@/Components/InputError.vue';
+    import InputLabel from '@/Components/InputLabel.vue';
+
+    defineProps({
+        canResetPassword: {
+            type: Boolean,
+        },
+        status: {
+            type: String,
+        },
+    });
+
+    const form = useForm({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const submit = () => {
+        form.post(route('login'), {
+            onFinish: () => form.reset('password'),
+        });
+    };
+</script>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,4 +1,139 @@
+<template>
+    <Head title="S'enregistrer" />
+
+    <AuthenticatedLayout>
+        <template #header>
+
+        </template>
+
+        <section class="w-full flex justify-center min-h-screen">
+            
+            <form @submit.prevent="submit" class="w-72">
+                <h1 class="font-bold text-2xl text-center mt-14 mb-12">S' enregistrer</h1>
+                <div class="mt-4">
+                    <InputLabel class="font-semibold" for="firstname" value="Prenom" />
+
+                    <input
+                        id="firstname"
+                        type="text"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.firstname"
+                        required
+                        autofocus
+                        autocomplete="firstname"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.name" />
+                </div>
+                <div class="mt-4">
+                    <InputLabel class="font-semibold" for="lastname" value="Nom" />
+
+                    <input
+                        id="lastname"
+                        type="text"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.lastname"
+                        required
+                        autofocus
+                        autocomplete="lastname"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.name" />
+                </div>
+                <div class="mt-4">
+                    <InputLabel class="font-semibold" for="email" value="Email" />
+
+                    <input
+                        id="email"
+                        type="email"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel class="font-semibold" for="password" value="Mot de passe" />
+
+                    <input
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel class="font-semibold" for="password_confirmation" value="Confirmer mot de passe" />
+
+                    <input
+                        id="password_confirmation"
+                        type="password"
+                        class="mt-1 block w-full h-9"
+                        v-model="form.password_confirmation"
+                        required
+                        autocomplete="new-password"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                </div>
+
+                <button class="w-full btn-primary mt-10 text-xl h-10" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    S' enregistrer
+                </button>
+
+                <div class="flex flex-col items-end justify-end mt-4">
+                    <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Deja un compte ?
+                    </Link>
+                </div>
+            </form>
+        </section>
+    </AuthenticatedLayout>
+</template>
+
 <script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head, Link, useForm } from '@inertiajs/vue3';
+    import Checkbox from '@/Components/Checkbox.vue';
+    import InputError from '@/Components/InputError.vue';
+    import InputLabel from '@/Components/InputLabel.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import TextInput from '@/Components/TextInput.vue';
+
+    defineProps({
+        canResetPassword: {
+            type: Boolean,
+        },
+        status: {
+            type: String,
+        },
+    });
+
+    const form = useForm({
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    });
+
+    const submit = () => {
+        form.post(route('register'), {
+            onFinish: () => form.reset('password', 'password_confirmation'),
+        });
+    };
+</script>
+
+<!-- <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
@@ -7,6 +142,7 @@ import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
+    name: '',
     name: '',
     email: '',
     password: '',
@@ -31,7 +167,7 @@ const submit = () => {
                 <TextInput
                     id="name"
                     type="text"
-                    class="mt-1 block w-full"
+                    class="mt-1 block w-full h-9"
                     v-model="form.name"
                     required
                     autofocus
@@ -47,7 +183,7 @@ const submit = () => {
                 <TextInput
                     id="email"
                     type="email"
-                    class="mt-1 block w-full"
+                    class="mt-1 block w-full h-9"
                     v-model="form.email"
                     required
                     autocomplete="username"
@@ -62,7 +198,7 @@ const submit = () => {
                 <TextInput
                     id="password"
                     type="password"
-                    class="mt-1 block w-full"
+                    class="mt-1 block w-full h-9"
                     v-model="form.password"
                     required
                     autocomplete="new-password"
@@ -77,7 +213,7 @@ const submit = () => {
                 <TextInput
                     id="password_confirmation"
                     type="password"
-                    class="mt-1 block w-full"
+                    class="mt-1 block w-full h-9"
                     v-model="form.password_confirmation"
                     required
                     autocomplete="new-password"
@@ -101,3 +237,4 @@ const submit = () => {
         </form>
     </GuestLayout>
 </template>
+ -->
